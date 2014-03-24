30c0622932bf06d0d8d3e0cdb614e8a668527d30
Jorge Valverde <horuxd@gmail.com>
2014-03-24 14:30:36 -0600

    Terminando la galería

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -10,52 +10,74 @@
 		var g = gallery, // g = the new gallery.
 			views = g.get('div[data-view]'),
 			thumb_wrapper = g.get('ul[data-thumbs]'),
+			images = g.get('img'),
 			// ----------------------------------- //
 			i = 0, l = views.length,
 			// ----------------------------------- //
 			currentView = 0,
-			animating = false;
+			userover = false;
 
 
 		for(; i<l; i++){
-			var thumb = create('li');
+			var thumb = create('li'),
+				img = create('img');
+
 
 			thumb_wrapper.append( thumb );
-			thumb.addEvent('click',changeView);
-		}
+			thumb.append( img );
 
-		for(i=1;i<l;i++){
-			views[i].addClass('gallery-right');
+			img.setAttributes({
+				'src' : images[i].src,
+				'alt' : images[i].alt,
+				'width': images[i].width,
+				'height' : 'auto',
+				'style' : 'right: '+images[i].data('thumb-view')
+			});
+
+			thumb.addEvent('click',changeView);
 		}
 
 		thumb_wrapper.get('li:first-child').addClass('active');
 
 		var thumbnails = thumb_wrapper.get('li');
 
+		var galleryInterval = w.setInterval(changeView,5000);
+
+		views.addEvent('mouseover', cancel_auto_gallery);
+		views.addEvent('mouseout', continue_auto_gallery);
+
+		function cancel_auto_gallery(){ userover = true; }
+
+		function continue_auto_gallery(){ userover = false;	}
+
 		function changeView(){
 
-			var view = thumbnails.indexOf( this ) || (currentView+1);
+			if( !(userover) ){
 
-			if( view >= thumbnails.length ){
-				view = 0;
+				var view = thumbnails.indexOf( this );
+
+				if( !(view) && view !== 0 || (this === w)){
+					view = currentView+1;
+				} 
+
+				if( view >= thumbnails.length ){
+					view = 0;
+				}
+
+				if( (view !== currentView) ){				
+
+
+					thumbnails.removeClass('active');
+					views[view].addClass('active');
+
+					views.removeClass('active');
+					views[ view ].addClass('active');
+
+					currentView = view;
+
+				}
+
 			}
-
-			if( !(animating) && (view !== currentView) ){				
-
-				animating = true;
-
-				thumbnails.removeClass('active');
-				this.addClass('active');
-
-				views.removeClass('active');
-				views[ view ].addClass('active');
-
-				currentView = view;
-
-			}
-
-			animating = false;
-
 		}
 
 	};
